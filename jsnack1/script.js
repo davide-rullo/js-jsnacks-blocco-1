4396425d6774c2e66424058a553863f334414950
davide-rullo <davide.rullo01@gmail.com>
2023-08-30 10:52:42 +0200

    add
    added bonus while loop

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,6 +1,6 @@
 /*Snack 1:
 L’utente inserisce due numeri in successione, con due prompt.
-Il software stampa il maggiore. */
+Il software stampa il maggiore. 
 
 const firstNumber = prompt("Inserisci il primo numero");
 const secondNumber = prompt("Inserisci il secondo numero");
@@ -11,4 +11,26 @@ if (firstNumber > secondNumber) {
     console.log("il numero maggiore è il secondo");
 } else {
     console.log("I numeri sono uguali");
+}
+*/
+
+//Soluzione con ciclo while
+
+let i = 0;
+let list = []
+
+while (i < 2) {
+    const number = prompt("Inserisci un numero");
+    list.push(number)
+    i++
+
+    if (list[0] > list[1]) {
+        console.log("il numero maggiore è il primo");
+    } else if (list[0] < list[1]) {
+        console.log("il numero maggiore è il secondo");
+    } else {
+        console.log("I numeri sono uguali");
+    }
+    
+    console.log(list)
 }
