136540d791347d3143d868acbafbc2ff49799ef0
davide-rullo <davide.rullo01@gmail.com>
2023-08-30 11:48:18 +0200

    add
    added while loop script

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -1,6 +1,6 @@
 /*Snack 2:
 L’utente inserisce due parole in successione, con due prompt.
-Il software stampa prima la parola più corta, poi la parola più lunga.*/
+Il software stampa prima la parola più corta, poi la parola più lunga.
 
 const firstWord = prompt("Inserisci la prima parola");
 const secondWord = prompt("Inserisci la seconda parola");
@@ -11,4 +11,24 @@ if (firstWord.length > secondWord.length) {
     console.log(firstWord, secondWord);
 } else {
     console.log(firstWord, secondWord);
+}
+*/
+
+//Soluzione con while
+
+let i = 0;
+let list = []
+
+while (i < 2) {
+    const parola = prompt("Inserisci una parola");
+    list.push(parola)
+    i++
+}
+
+if (list[0] > list[1]) {
+    console.log(list[1], list[0]);
+} else if (list[0] < list[1]) {
+    console.log(list[0], list[1]);
+} else {
+    console.log(list[0], list[1]);
 }
